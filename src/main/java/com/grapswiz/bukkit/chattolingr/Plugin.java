7a813fe49c8308cf5f4995688771e0378fa430c5
grapswiz <ineeza@gmail.com>
2014-01-24 23:44:57 +0900

    Pluginとして実行可能なように書いてみた

---
+++ src/main/java/com/grapswiz/bukkit/chattolingr/Plugin.java
@@ -0,0 +1,8 @@
+package com.grapswiz.bukkit.chattolingr;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Plugin extends JavaPlugin implements  Listener {
+
+}
